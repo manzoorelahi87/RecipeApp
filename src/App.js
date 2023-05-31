572dd3174eee76f4ad2f4d0672383a2df5c235d2
Manzoor Elahi C M <mazoor.cm@gmail.com>
2023-05-31 08:11:30 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useEffect, useState } from 'react'
+import './App.css';
+import Recipe from './pages/Recipe';
+import Header from './components/Header'
+
+const App = () => {
+  const APP_ID = "4f7e69ea";
+  const APP_KEY = "9c9e651c164491605a6f6510652cfa6a";
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+  useEffect(() => {
+    getRecipes();
+  }, [query])
+  const getRecipes = async () => {
+    const response = await fetch
+          (`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+ 
+  
+  };
+
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header  />
+      <form className="search-form" onSubmit={getSearch}  >
+        <input className="search-bar" type="text" value={search}
+             onChange={updateSearch} />
+        <button className="search-button" type="submit" >
+             Search
+        </button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />  
+        ))}
+      </div>
+  
     </div>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,5 @@
+const Login = () => {
+    return <h1>Enter your login details:</h1>;
+  };
+  
+  export default Login;
